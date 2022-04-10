e364b1c63ff23395cfde3e00c949a0ddf6d07dff
VITALIY <vamelcore@gmail.com>
2022-04-10 18:08:38 +0300

    Fix: routing to SPA index page

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::any('{any}', function () {
+    return view('index');
+})->where('any', '^(?!api).*$');
