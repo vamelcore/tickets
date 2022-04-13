e236102a573ef79b71e4e959d51613490c0658e1
VITALIY <vamelcore@gmail.com>
2022-04-14 02:16:10 +0300

    Feat: Api error 404

--- routes/api.php
+++ routes/api.php
@@ -14,4 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-//Route::any('/', [\App\Http\Controllers\ApiVersionController::class, 'index']);
+Route::fallback(function(){
+    return abort(404, 'Resource Not Found.');
+});
