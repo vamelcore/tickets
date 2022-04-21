2aa1bfa0e422a2a9cd32cff5530b659295db2894
VITALIY <vamelcore@gmail.com>
2022-04-21 22:49:22 +0300

    Feat: added PasswordHelper with randString method

---
+++ app/Helpers/PasswordHelper.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Helpers;
+
+class PasswordHelper
+{
+    public static function randString( $length = 12 ) {
+        $chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        return substr(str_shuffle(str_repeat($chars, $length)), rand(0, strlen($chars) * $length - $length), $length);
+    }
+}
